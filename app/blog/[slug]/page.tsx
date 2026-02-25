50719525979b21382dc3e0478881e0df4d9d7e32
kyle-hensman <30512589+kyle-hensman@users.noreply.github.com>
2026-02-25 14:47:40 -0400

    Feat/add sanity cms (#2)
    * feat: add shadcn library
    
    * feat: adds sanity cms integration
    
    * fix: fixed suggestions from code review
    
    * fix: more minor revisions for code review
    
    * fix: update image tage to next/image component
    
    * fix: suggested updates to readme and sanity env file
    
    * fix: minor updates to readme file as per code review

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import { PortableText, type SanityDocument } from "next-sanity";
+import { client } from "@/sanity/lib/client";
+import Link from "next/link";
+import { urlFor } from "@/sanity/lib/image";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+
+const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const post = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
+  const postImageUrl = post.image
+    ? urlFor(post.image)?.width(550).height(310).url()
+    : null;
+
+  if (!post) {
+    return notFound();
+  }
+
+  return (
+    <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
+      <Link href="/blog" className="hover:underline">
+        ← Back to posts
+      </Link>
+      {postImageUrl && (
+        <Image
+          src={postImageUrl}
+          alt={post.title}
+          className="aspect-video rounded-xl"
+          width="550"
+          height="310"
+        />
+      )}
+      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+      <div className="prose">
+        {post.publishedAt && (
+          <p>Published: {new Date(post.publishedAt).toLocaleDateString("en-US")}</p>
+        )}
+        {Array.isArray(post.body) && <PortableText value={post.body} />}
+      </div>
+    </main>
+  );
+}
